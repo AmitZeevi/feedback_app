04a5803df7bde98bdd4dfc5f499bc1b0a5f87258
Amit Zeevi <amitz@wix.com>
2022-08-06 16:53:26 +0300

    up to step section 14

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback }) {
+    if(!feedback || feedback.length === 0){
+        return "No feedback yet"
+    }
+
+  return (
+    <div className="feedback-list">
+        {feedback.map((item) =>(
+            <div> {item.rating} </div>
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
